6a6513b127e93d9fc2cd12e5f26627d3a2e8e0a8
Dieter Stokar <inbox@usility.ch>
2021-11-29 15:33:28 +0100

    config.php: - bugfix -> determine whether on localhost to auto-enable debug

--- install/site/config/config.php
+++ install/site/config/config.php
@@ -1,9 +1,12 @@
 <?php
 
-
+// to simply first steps, debug is automatically enabled on localhost:
 return [
-    'debug'  => function() {
-        $remoteAddress = isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 'REMOTE_ADDR';
-        return (($remoteAddress == 'localhost') || (strpos($remoteAddress, '192.') === 0) || ($remoteAddress == '::1'));
-    },
+    'debug' => isLocalhost(),
 ];
+
+function isLocalhost()
+{
+    $remoteAddress = isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 'REMOTE_ADDR';
+    return (($remoteAddress == 'localhost') || (strpos($remoteAddress, '192.') === 0) || ($remoteAddress == '::1'));
+}
