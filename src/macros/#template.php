eb605c38359325ad2a4f38f6e3b05ae98b76e876
Dieter Stokar <inbox@usility.ch>
2021-11-29 13:30:05 +0100

    #template.php: - template for macros added

---
+++ src/macros/#template.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Usility\PageFactory\Macros;
+
+$macroConfig =  [
+    'name' => strtolower( $macroName ),
+    'parameters' => [
+        '' => ['', false],
+    ],
+    'mdCompile' => false,
+    'wrapInComment' => true,
+    'summary' => <<<EOT
+[Short description of macro.]
+EOT,
+];
+
+
+
+class >replace with Classname< extends Macros
+{
+    public static $inx = 1;
+    public function __construct($pfy = null)
+    {
+        $this->name = strtolower(__CLASS__);
+        parent::__construct($pfy);
+    }
+
+
+    public function render($args, $argStr)
+    {
+        $inx = self::$inx++;
+
+        $arg = $args['arg'];
+        $str = '';
+        return$str;
+    }
+}
+
+
+
+
+
+// ==================================================================
+$macroConfig['macroObj'] = new $thisMacroName($this->pfy);
+return $macroConfig;
