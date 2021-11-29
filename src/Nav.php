b1f9ab5b20b5a7708566a28b9c0007d0ff643449
Dieter Stokar <inbox@usility.ch>
2021-11-29 15:34:08 +0100

    Nav: - code cleanup - case empty site -> render empty string

--- src/Nav.php
+++ src/Nav.php
@@ -1,14 +1,9 @@
 <?php
 
-//namespace Usility\PageFactory\Nav;
-
-//use Kirby\Cms\App as Kirby;
 namespace Usility\PageFactory;
 
-define('NAV_ARROW', '<span>&#9727;</span>'); // '&#9657;'); // '&#9013;'; // '&#9657;'; //'&#9656;';
-//define('NAV_ARROW', '&#9727;'); // '&#9657;'); // '&#9013;'; // '&#9657;'; //'&#9656;';
-//define('NAV_ARROW', '&#9657;'); // '&#9657;'); // '&#9013;'; // '&#9657;'; //'&#9656;';
-//define('NAV_ARROW', '<span class=\'lzy-icon lzy-icon-triangle\'></span>'); // '&#9657;'); // '&#9013;'; // '&#9657;'; //'&#9656;';
+define('NAV_ARROW', '<span>&#9727;</span>');
+
 
 class Nav
 {
@@ -28,6 +23,9 @@ class Nav
     {
         $elem = $this->site->children();
         $out = $this->_render($elem);
+        if (!$out) {
+            return '';
+        }
 
         $out = <<<EOT
 <div id='lzy-primary-nav' class='lzy-nav-wrapper lzy-primary-nav'>
@@ -36,27 +34,6 @@ $out
     </nav>
 </div>
 EOT;
-
-//    $nav = $this->site->children()->listed();
-//    $out = '';
-//    foreach ($nav as $elem) {
-//      $x = $elem->toStructure();
-//      $url = $elem->url();
-//      $name = $elem->title()->html();
-//      $liClass = 'lzy-lvl1';
-////      $liClass = 'lzy-lvl1 lzy-curr lzy-active';
-//      $out .= "\t<li class='$liClass'>><a href='$url'>$name</a></li>\n";
-//    }
-//
-//    $out = <<<EOT
-//<div id='lzy-primary-nav' class='lzy-nav-wrapper lzy-primary-nav'>
-//	  <nav class='lzy-nav lzy-nav-colored lzy-nav-top-horizontal lzy-nav-indented lzy-nav-accordion lzy-nav-collapsed lzy-nav-animated lzy-nav-hoveropen lzy-encapsulated lzy-nav-small-tree'>
-//		<ol>
-//$out
-//    </ol>
-//    </nav>
-//</div>
-//EOT;
         return $out;
     } // render
 
@@ -64,11 +41,7 @@ EOT;
 
     private function _render($subtree)
     {
-//        $depth = $subtree->depth();
-//        if ($depth > 1) {
-//            $out = "\t<ol style='margin-top: -10000px'>\n";
-//        }
-        $out = "\t<ol>\n";
+        $out = '';
         foreach ($subtree->listed() as $p) {
             $depth = $p->depth();
             $indent = str_repeat('    ', $depth);
@@ -85,11 +58,9 @@ EOT;
             $title = $p->title()->html();
             $hasChildren = !$p->children()->listed()->isEmpty();
             if ($hasChildren) {
-//            if ($p->hasChildren()) {
                 $aElem = "<span class='lzy-nav-label'>$title</span><span class='lzy-nav-arrow' aria-hidden='true'>{$this->arrow}</span>";
                 $class .= ' lzy-has-children lzy-nav-has-content';
                 $out .= "\t$indent<li class='$class'><a href='$url'>$aElem</a>\n";
-//                $out .= "\t$indent<li class='$class'><a href='$url'>$title</a>\n";
                 $out .= "\t$indent  <div class='lzy-nav-sub-wrapper' aria-hidden='true'>\n";
                 $out .= $this->_render($p->children());
                 $out .= "\t$indent  </div>\n";
@@ -98,6 +69,10 @@ EOT;
                 $out .= "\t$indent<li class='$class'><a href='$url'>$title</a></li>\n";
             }
         }
+        if (!$out) {
+            return '';
+        }
+        $out = "\t<ol>\n".$out;
         $out .= "\t</ol>\n";
         return $out;
     }
