edf77b75ee6026c9a6ce62e693c179891a206081
Dieter Stokar <inbox@usility.ch>
2021-11-29 11:53:12 +0100

    install.sh: sample installation script -> clones Kirby and PageFactory plugin, copies essential files to final location

---
+++ install/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# assumes you are in your new web-app's root folder:
+
+
+
+## Check/clone Kirby plainkit:
+if [ ! -e kirby/ ]; then
+	/usr/local/bin/git clone https://github.com/getkirby/plainkit.git
+	echo Kirby installed
+else
+	echo Kerby already installed
+fi
+
+
+## Clone PageFactory:
+/usr/local/bin/git clone  git@github.com:pgfactory/kirby-pagefactory-plugin site/plugins/pagefactory
+echo PageFactory installed
+
+
+## Check/copy essential files to final location:
+if [ ! -e site/templates/page_template.html ]; then
+	cp -R site/plugins/pagefactory/install/site/*  site
+	cp -R site/plugins/pagefactory/install/content/*  content
+	echo Essential files to copied final location
+fi
