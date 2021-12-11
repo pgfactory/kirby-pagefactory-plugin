7e05b0a883f276e1dd4d0a68d9e5e1d4a2961507
Dieter Stokar <inbox@usility.ch>
2021-12-11 11:05:17 +0100

    installation: improvements - index.php

--- install/site/templates/default.php
+++ install/site/templates/default.php
@@ -1,7 +1,21 @@
 <?php
 
-echo $page->pageFactoryRender( $pages );
+ob_start();
 
+$html = $page->pageFactoryRender($pages, 'page_template.html' );
 
-// or if page needs specific template:
-//echo $page->pageFactoryRender( $pages, 'template.html' );
+if (ob_get_length()) {
+    if (!file_exists(PFY_LOGS_PATH)) {
+        mkdir(PFY_LOGS_PATH);
+    }
+    file_put_contents('site/log/prematureOutput.txt', ob_get_contents());
+    ob_clean();
+}
+
+// if HTML contains <?php tags, we need to get that evaluated:
+if (strpos($html, '<?') !== false) {
+    eval(" ?>$html<?php ");
+} else {
+    echo $html;
+}
+
