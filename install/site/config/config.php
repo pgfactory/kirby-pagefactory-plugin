2509c5620d95b2594d181a85141509e833853fd8
Dieter Stokar <inbox@usility.ch>
2021-12-13 10:19:29 +0100

    Icons: - moved to install/media/pagefactory/icons/ -> now they are available for use in approot/media/pagefactory/icons/
    Languages:
    - template in install/site/languages/ added
    - config.php -> language = true activated by default

--- install/site/config/config.php
+++ install/site/config/config.php
@@ -4,7 +4,7 @@
 return [
 // uncomment as desired:
 //     'smartypants' => true,
-//     'languages' => true,
+    'languages' => true,
 //     'auth' => [
 //         'methods' => ['password','code']
 //     ],
