052a9196f7cf575fb9894bc06c7e12e30c447538
Dieter Stokar <inbox@usility.ch>
2021-12-19 15:48:35 +0100

    helper.js: - unTransvar() added

--- js/autoload/helper.js
+++ js/autoload/helper.js
@@ -109,3 +109,18 @@ function appendToUrl(url, arg) {
     }
     return url;
 } // appendToUrl
+
+
+
+function unTransvar( str ) {
+    // looks for '{{ lzy-... }}' patterns, removes them.
+    // Note: if site_enableFilesCaching is active, transvars will already be translated at this point,
+    // so, this is just a fallback to beautify output during dev time
+    if ( str.match(/{{/)) {
+        // need to hide following line ('{{...}}') from being translated when preparing cache:
+        const patt = String.fromCharCode(123) + '{\\s*(lzy-)?(.*?)\\s*' + String.fromCharCode(125) + '}';
+        const re = new RegExp( patt, 'g');
+        str = str.replace(re, '$2');
+    }
+    return str;
+} // unTransvar
