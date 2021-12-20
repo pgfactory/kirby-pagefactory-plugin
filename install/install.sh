17a5f84f49338a18fdde274e4dad1d7d4a8cdeca
Dieter Stokar <inbox@usility.ch>
2021-12-20 08:40:09 +0100

    installation script and instructions updated

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-# assumes you are in your new web-app's root folder:
+# assumes you are in your new web-app's root folder.
 
+## select the branch you want to check out:
+branch=''
+#branch='-b Dev'
 
 
 ## Check/clone Kirby plainkit:
 if [ ! -e kirby/ ]; then
-	/usr/local/bin/git clone https://github.com/getkirby/plainkit.git
+	/usr/local/bin/git clone https://github.com/getkirby/plainkit.git .
 	echo Kirby installed
 else
 	echo Kerby already installed
@@ -13,15 +16,15 @@ fi
 
 
 ## Clone PageFactory:
-/usr/local/bin/git clone  git@github.com:pgfactory/kirby-pagefactory-plugin site/plugins/pagefactory
+/usr/local/bin/git clone $branch git@github.com:pgfactory/kirby-pagefactory-plugin site/plugins/pagefactory
 echo PageFactory installed
 
 
 ## Check/copy essential files to final location:
 if [ ! -e site/templates/page_template.html ]; then
-	cp -R site/plugins/pagefactory/install/site/*  site
 	cp -R site/plugins/pagefactory/install/content/*  content
-	cp -R site/plugins/pagefactory/install/media/*  media
+	cp -R site/plugins/pagefactory/install/media/*    media
+	cp -R site/plugins/pagefactory/install/site/*     site
 	echo Essential files copied to final location
 fi
 
