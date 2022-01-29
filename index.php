8866c9f399c23772ae10c42ed24181c215b75f01
Dieter Stokar <zurisee@gmail.com>
2022-01-29 19:32:56 +0100

    index.php (in pagefactory): - routing -> fall-through to first child page in case of md-files missing in folder

--- index.php
+++ index.php
@@ -18,6 +18,26 @@ use Kirby\Cms\App as Kirby;
 use Usility\PageFactory\PageFactory as PageFactory;
 
 
+/**
+ * @param string $path
+ * @return bool
+ */
+function hasMdContent(string $path): bool
+{
+    $mdFiles = glob("$path/*.md");
+    $hasContent = false;
+    if ($mdFiles && is_array($mdFiles)) {
+        foreach ($mdFiles as $file) {
+            if (basename($file)[0] !== '#') {
+                $hasContent = true;
+            }
+        }
+    }
+    return $hasContent;
+}
+
+
+
 Kirby::plugin('usility/pagefactory', [
     'pageMethods' => [
         'pageFactoryRender' => function($pages, $options = false) {
@@ -27,14 +47,33 @@ Kirby::plugin('usility/pagefactory', [
 
     'routes' => [
         [
-            // catch tokens in URLs: (i.e. all capital letter codes like 'p1/ABCDEF')
+            // catch tokens in URLs: (i.e. all capital letter codes like 'p1/ABCDEF'):
             'pattern' => '(:all)',
             'action'  => function ($slug) {
+                $doDivert = false;
+
+                // check for token in url (i.e access token), extract it
                 if (preg_match('|^(.*?) / ([A-Z]{5,15})$|x', $slug, $m)) {
                     $slug = $m[1];
-                    return site()->visit($slug);
+                    $doDivert = true;
                 }
-                $this->next();
+
+                // if folder contains no md-file, fall through to first child page:
+                $page = page($slug);
+                if ($page) {
+                    $path = $page->root();
+                    $hasContent = hasMdContent($path);
+                    if (!$hasContent && $page->hasListedChildren()) {
+                        $page = $page->children()->listed()->first();
+                        $doDivert = true;
+                    }
+                }
+
+                if ($doDivert) {
+                    return site()->visit($page);
+                } else {
+                    $this->next();
+                }
             }
         ],
     ],
