52fc4b30ef2db31a87f17dbb620bc87683f52a56
Dieter Stokar <zurisee@gmail.com>
2022-01-29 19:35:07 +0100

    PageFactory: - minor refactoring

--- src/PageFactory.php
+++ src/PageFactory.php
@@ -126,37 +126,8 @@ class PageFactory
         }
 
         $this->siteTitle = (string)site()->title()->value();
+        $this->determineAssetFilesList();
 
-        // get asset-files definition: first try site/config/pagefactory.php:
-        if (@$this->config['assetFiles']) {
-            $this->assetFiles = $this->config['assetFiles'];
-        } else { // if not found, use following as default values:
-            $this->assetFiles = [
-                '-pagefactory.css' => [
-                    'site/plugins/pagefactory/scss/autoload/*',
-                ],
-                '-pagefactory-async.css' => [
-                    'site/plugins/pagefactory/scss/autoload-async/*',
-                ],
-                '-styles.css' => [
-                    PFY_USER_ASSETS_PATH . 'autoload/*',
-                ],
-                '-styles-async.css' => [
-                    PFY_USER_ASSETS_PATH . 'autoload-async/*',
-                ],
-
-                '-pagefactory.js' => [
-                    'site/plugins/pagefactory/js/autoload/*',
-                ],
-
-                // prepare rest as individual files ready for explicit queueing/loading:
-                '*' => [
-                    'site/plugins/pagefactory/scss/*',
-                    'site/plugins/pagefactory/third_party/jquery/jquery-3.6.0.min.js',
-                    'site/plugins/pagefactory/js/*',
-                ],
-            ];
-        }
         preparePath(PFY_LOGS_PATH);
     } // __construct
 
@@ -291,6 +262,43 @@ class PageFactory
     public static function instance()
     {
         return static::$instance ?? new static(pages());
+    }
+
+    /**
+     * @return void
+     */
+    private function determineAssetFilesList(): void
+    {
+// get asset-files definition: first try site/config/pagefactory.php:
+        if (@$this->config['assetFiles']) {
+            $this->assetFiles = $this->config['assetFiles'];
+        } else { // if not found, use following as default values:
+            $this->assetFiles = [
+                '-pagefactory.css' => [
+                    'site/plugins/pagefactory/scss/autoload/*',
+                ],
+                '-pagefactory-async.css' => [
+                    'site/plugins/pagefactory/scss/autoload-async/*',
+                ],
+                '-styles.css' => [
+                    PFY_USER_ASSETS_PATH . 'autoload/*',
+                ],
+                '-styles-async.css' => [
+                    PFY_USER_ASSETS_PATH . 'autoload-async/*',
+                ],
+
+                '-pagefactory.js' => [
+                    'site/plugins/pagefactory/js/autoload/*',
+                ],
+
+                // prepare rest as individual files ready for explicit queueing/loading:
+                '*' => [
+                    'site/plugins/pagefactory/scss/*',
+                    'site/plugins/pagefactory/third_party/jquery/jquery-3.6.0.min.js',
+                    'site/plugins/pagefactory/js/*',
+                ],
+            ];
+        }
     }
 
 } // PageFactory
