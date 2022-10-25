35d91f3ab0a7e6261e5b626b9143c6a0189ba99e
Dieter Stokar <inbox@usility.ch>
2022-10-25 14:36:23 +0200

    installation scripts: - split in install.sh  => install all and install-pagefactory.sh  => install Pagefactory into existing Kirby installation

---
+++ install/install-pagefactory.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# Usage:
+#   install-pagefactory.sh {branch}
+#
+# if arg is omitted, script assumes you already navigated to your new web-app folder.
+
+## select the branch you want to check out:
+if [[ -z "$1" ]]; then
+	branch=''
+else
+	branch="-b $1"
+fi
+
+## Check Kirby plainkit:
+if [[ ! -e kirby/ ]]; then
+	echo No Kirby installation found - please install Kirby first
+	exit
+fi
+
+## Check Kirby plainkit:
+if [[ -e site/plugins/pagefactory/ ]]; then
+	echo Pagefactory already installed
+	echo 
+	exit
+fi
+
+echo Now installing Pagefactory
+
+## Clone PageFactory:
+/usr/local/bin/git clone $branch https://github.com/pgfactory/kirby-pagefactory-plugin.git site/plugins/pagefactory
+echo PageFactory installed
+
+
+## Check/copy essential files to final location:
+if [ ! -e site/templates/page_template.html ]; then
+	cp -R site/plugins/pagefactory/install/assets/   assets
+	cp -R site/plugins/pagefactory/install/content/  content
+	cp -R site/plugins/pagefactory/install/site/     site
+	cp content/home/home.txt content/home/zzz_page.txt
+	mv content/home/home.txt content/home/zzz_page.en.txt
+	cp content/site.txt content/site.en.txt
+	mv content/home content/1_home
+	echo Essential files copied to final location
+fi
+
+echo
+echo Now open this website in your browser.
+echo 
--- install/install.sh
+++ install/install.sh
@@ -6,44 +6,39 @@
 # NOTE: in that case the folder must be empty, i.e. the install.sh script must
 # be stored somewhere else.
 
-if [ "$1" != "" ]; then
-	mkdir $1
+if [[ -z "$1" ]]; then
+	echo "Usage:"
+	echo   install.sh path-to-webapp-folder
+	echo
+	exit
+fi
+
+cwd=`pwd`
+if [[ "$1" != "." ]]; then
+	if [[ ! -e $1 ]]; then
+		mkdir $1
+	fi
+	if [[ ! -d $1 ]]; then
+		echo Target folder could not be created
+		echo
+		exit
+	fi
 	cd $1
 fi
 
-echo "Installing to folder -> `pwd`"
-
-
-## select the branch you want to check out:
-branch=''
-# branch='-b Dev'
+if [[ -n "$(ls -A)" &&  ! -d kirby/ ]]; then
+   echo "Not empty - to install Kirby, folder must be empty"
+   exit
+fi
 
 
 ## Check/clone Kirby plainkit:
-if [ ! -e kirby/ ]; then
+if [[ ! -e kirby/ ]]; then
+	echo "Now installing Kirby to folder -> `pwd`"
 	/usr/local/bin/git clone https://github.com/getkirby/plainkit.git .
 	echo Kirby installed
 else
 	echo Kirby already installed
 fi
 
-
-## Clone PageFactory:
-/usr/local/bin/git clone $branch https://github.com/pgfactory/kirby-pagefactory-plugin.git site/plugins/pagefactory
-echo PageFactory installed
-
-
-## Check/copy essential files to final location:
-if [ ! -e site/templates/page_template.html ]; then
-	cp -R site/plugins/pagefactory/install/assets/   assets
-	cp -R site/plugins/pagefactory/install/content/  content
-	cp -R site/plugins/pagefactory/install/site/     site
-	cp content/home/home.txt content/home/zzz_page.txt
-	mv content/home/home.txt content/home/zzz_page.en.txt
-	cp content/site.txt content/site.en.txt
-	mv content/home content/1_home
-	echo Essential files copied to final location
-fi
-
-echo
-echo Now open this website in your browser.
+$cwd/install-pagefactory.sh $2
