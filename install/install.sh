55b0036588c62d634b319e8372214b2491c119b5
Dieter Stokar <inbox@usility.ch>
2022-10-26 17:17:21 +0200

    install.sh: - cont

--- install/install.sh
+++ install/install.sh
@@ -41,7 +41,38 @@ else
 	echo Kirby already installed
 fi
 
-# Invoke Pagefactory installation script (assumed to be in same location as install.sh):
-thisScript=$0
-path=${thisScript%/*}
-$cwd/$path/install-pagefactory.sh $2
+
+## select the branch you want to check out:
+if [[ -z "$2" ]]; then
+	branch=''
+else
+	branch="-b $2"
+fi
+
+## Check pagefactory:
+if [[ -e site/plugins/pagefactory/ ]]; then
+	echo Pagefactory already installed
+	echo 
+	exit
+fi
+
+echo Now installing Pagefactory
+
+## Clone PageFactory:
+/usr/local/bin/git clone $branch https://github.com/pgfactory/kirby-pagefactory-plugin.git site/plugins/pagefactory
+echo PageFactory installed
+
+
+## Check/copy essential files to final location:
+if [ ! -e site/templates/page_template.html ]; then
+	cp -R site/plugins/pagefactory/install/assets/   assets
+	cp -R site/plugins/pagefactory/install/content/  content
+	cp -R site/plugins/pagefactory/install/site/     site
+	mv content/home/home.txt content/home/zzz_page.txt
+	mv content/home content/1_home
+	echo Essential files copied to final location
+fi
+
+echo
+echo Now open this website in your browser.
+echo 
