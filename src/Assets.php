a55e250664adbee394320841ecaf9a685a3732b7
Dieter Stokar <inbox@usility.ch>
2022-12-19 10:55:37 +0100

    Macros: major overhaul cont

--- src/Assets.php
+++ src/Assets.php
@@ -45,10 +45,10 @@ const ASSET_URL_DEFINITIONS = [
         'site/plugins/pagefactory/assets/js/quickview.js',
         'site/plugins/pagefactory/assets/css/-quickview.css',
     ],
-    'REVEAL' => [
-        'site/plugins/pagefactory/assets/js/reveal.js',
-        'site/plugins/pagefactory/assets/css/-reveal.css',
-    ],
+//    'REVEAL' => [
+//        'site/plugins/pagefactory/assets/js/reveal.js',
+//        'site/plugins/pagefactory/assets/css/-reveal.css',
+//    ],
     'PAGE_SWITCHER' => [
         'site/plugins/pagefactory/assets/css/-page-switcher.css',
         'site/plugins/pagefactory/assets/js/page-switcher.js',
