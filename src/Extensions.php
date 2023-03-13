363843d8a734f8ee95b0f7a4b8c7a2409e970ab6
Dieter Stokar <inbox@usility.ch>
2023-03-13 19:58:56 +0100

    Extensions: - code cleanup

--- src/Extensions.php
+++ src/Extensions.php
@@ -26,8 +26,6 @@ class Extensions
         // check for and load extensions:
         if (self::$availableExtensions) {
             foreach (self::$availableExtensions as $extPath) {
-//        if (PageFactory::$availableExtensions) {
-//            foreach (PageFactory::$availableExtensions as $extPath) {
                 // look for 'src/index.php' within the extension:
                 $indexFile = "{$extPath}src/index.php";
                 if (!file_exists($indexFile)) {
@@ -36,33 +34,23 @@ class Extensions
                 // === load index.php now:
                 $extensionClassName = require_once $indexFile;
                 self::$loadedExtensions[$extensionClassName] = $extPath;
-//                PageFactory::$loadedExtensions[$extensionClassName] = $extPath;
 
                 // instantiate extension object:
                 $extensionClass = "Usility\\PageFactoryElements\\$extensionClassName";
                 $obj = new $extensionClass();
                 self::$loadedExtensionObjects[] = $obj;
-//                $obj = new $extensionClass($this->pfy);
 
                 // check for and load extension's asset-definitions:
                 if (method_exists($obj, 'getAssetDefs')) {
                     $newAssets = $obj->getAssetDefs();
                     Page::$definitions = array_merge_recursive(Page::$definitions, ['assets' => $newAssets]);
-//                    self::$definitions = array_merge_recursive(self::$definitions, ['assets' => $newAssets]);
                 }
 
                 // check for and load extension's asset-definitions:
                 if (method_exists($obj, 'getAssetGroups')) {
                     $newAssetGroupss = $obj->getAssetGroups();
-//                    Page::$assets->addAssetGroups($newAssetGroupss);
                     PageFactory::$assets->addAssetGroups($newAssetGroupss);
                 }
-
-//                $file = $extPath.'src/_postProcessing.php';
-//                if (file_exists($file)) {
-//                    require_once $file;
-//                }
-
             }
         }
     } // loadExtensions
@@ -75,26 +63,11 @@ class Extensions
     public static function extensionsFinalCode(): void
     {
         foreach (self::$loadedExtensions as $path) {
-//        foreach (PageFactory::$loadedExtensions as $path) {
             $file = $path.'src/_finalCode.php';
             if (file_exists($file)) {
                 require_once $file;
             }
         }
     } // extensionsFinalCode
-
-
-
-//    public static function postProcessMd(string $html, $inx = null, $wrapperTag = null, $wrapperClass = null): array
-//    {
-//        $topWrapperClass = '';
-//        foreach (self::$loadedExtensionObjects as $obj) {
-//            if (method_exists($obj, 'postProcessMd')) {
-//                list($html, $cls, $inx) = $obj::postProcessMd($html, $inx, $wrapperTag, $wrapperClass);
-//                $topWrapperClass = trim("$wrapperClass $cls");
-//            }
-//        }
-//        return [$html, $topWrapperClass, $inx];
-//    } // extensionsFinalCode
 
 } // Extensions
