091a104f339941723c45922aceacda294d68d7fd
Dieter Stokar <inbox@usility.ch>
2024-02-18 14:59:50 +0100

    scss & js: - feature 'copy-to-clipboard button'

--- assets/js/autoload/helper.js
+++ assets/js/autoload/helper.js
@@ -123,3 +123,78 @@ function translateVar(transvarDef) {
 } // translateVar
 
 
+
+
+// === copy content button ==============================
+// targets for copy button are identified by class .pfy-has-copy-btn.
+function initCopyButton() {
+  const copyBtnElems = document.querySelectorAll('.pfy-has-copy-btn');
+  if (!copyBtnElems.length) {
+    return; // no copy buttons found
+  }
+
+  let inx = 0;
+  // loop over targets:
+  copyBtnElems.forEach(function (targetEl) {
+    inx++;
+    // prepare button element:
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = '<div>⎘</div>';
+    btnDiv.classList.add('pfy-copy-btn','pfy-button');
+
+    // wrap target in DIV.pfy-has-copy-btn
+    const parentEl = targetEl.parentElement;
+    targetEl.classList.remove('pfy-has-copy-btn');
+    targetEl.classList.add('pfy-copy-container');
+    const html = targetEl.outerHTML;
+    targetEl.outerHTML = '<div class="pfy-has-copy-btn pfy-has-copy-btn-'+inx+'">'+html+'</div>';
+
+    // append button to wrapper:
+    const wrapper = parentEl.querySelector('.pfy-has-copy-btn-' + inx);
+    wrapper.appendChild(btnDiv);
+  });
+
+// set up event handler for copy buttons:
+  const copyButtons = document.querySelectorAll('.pfy-copy-btn');
+  if (copyButtons) {
+    // loop over copy buttons:
+    copyButtons.forEach(function (btnEl) {
+      btnEl.addEventListener('click', function (e) {
+        const containerEl = this.parentElement.querySelector('.pfy-copy-container');
+        // get content:
+        let  txt = containerEl.value;
+        if (typeof txt === 'undefined') {
+          txt = containerEl.innerText;
+        }
+        // if content not empty, copy it to clipboard:
+        if (txt) {
+          copyToClipboard(txt, containerEl);
+        }
+      });
+    });
+  }
+
+// copy to clipboard
+  async function copyToClipboard(str, textareaEl) {
+    try {
+      // write to clipboard:
+      await navigator.clipboard.writeText(str);
+      mylog(`Copied to clipboard: "${str}"`);
+
+      // flash the element for user feedback:
+      const container = textareaEl.closest('.pfy-has-copy-btn');
+      container.classList.add('pfy-flash-copied');
+      setTimeout(function() {
+        container.classList.remove('pfy-flash-copied');
+      }, 1000);
+    } catch (err) {
+      mylog('Failed to copy: ' + err);
+    }
+  } // copyToClipboard
+  mylog('Copy button(s) initialized.');
+} // initCopyButton
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  initCopyButton();
+});
