74c560a0e5a08cb2e99d3a7e0403b96b8c35e214
Dieter Stokar <inbox@usility.ch>
2024-06-16 02:28:56 +0200

    macro vgap(): - option 'line'  added

--- macros/vgap.php
+++ macros/vgap.php
@@ -12,6 +12,7 @@ return function ($argStr = '')
         'options' => [
             'gapSize' => ['Height of inserted space. Use any form allowed in CSS, e.g. 3em, 20px or 1cm.', '1em'],
             'class' => ['Class applied to DIV', ''],
+            'line' => ["[true or border-style] Draws a horizontal line, e.g. line:'0.5em dashed #faa'", null],
         ],
         'summary' => <<<EOT
 # vgap()
@@ -31,6 +32,12 @@ EOT,
     // assemble output:
     $gapSize = $options['gapSize'];
     $class = $options['class'];
+    $line = $options['line'];
+    if ($line === true) {
+        $line = 'border-bottom: 1px solid black;';
+    } elseif ($line) {
+        $line = "border-bottom: $line";
+    }
 
     if (preg_match('/^[\d.]+$/', $gapSize)) {
         $gapSize = ($gapSize / 2).'px';
@@ -38,6 +45,6 @@ EOT,
         $gapSize = ($m[1] / 2).$m[2];
     }
     $class = trim("pfy-vgap pfy-vgap-{$inx} $class");
-    return "<div id='pfy-vgap-$inx' class='$class' style='margin:$gapSize 0;'>&nbsp;</div>";
+    return "<div id='pfy-vgap-$inx' class='$class' style='margin:$gapSize 0;$line'>&nbsp;</div>";
 };
 
