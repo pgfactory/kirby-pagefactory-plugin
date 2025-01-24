c0df3bc978ead4679e379ce711d88082304b3edd
Dieter Stokar <sto@usility.ch>
2025-01-24 08:28:10 +0100

    Utils: - fix re menu icon -> aria-label added

--- src/Utils.php
+++ src/Utils.php
@@ -229,11 +229,12 @@ class Utils
     {
         self::$menuIcon = $menuIcon = self::renderPfyIcon('menu');
         $smallScreenTitle = TransVars::$variables['smallScreenHeader']?? site()->title()->value();
+        $smallScreenMenuLabel = TransVars::$variables['smallScreenMenuLabel']??'Menu';
         $smallScreenHeader = <<<EOT
 
 <div class="pfy-small-screen-header pfy-small-screen-only">
     <h1>$smallScreenTitle</h1>
-    <button id='pfy-nav-menu-icon' type="button">$menuIcon</button>
+    <button id='pfy-nav-menu-icon' type="button"aria-label="$smallScreenMenuLabel">$menuIcon</button>
 </div>
 EOT;
 
