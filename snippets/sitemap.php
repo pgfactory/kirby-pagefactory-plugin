88210eb46c2445d4c455f886265bf35f43c470a0
Dieter Stokar <zurisee@gmail.com>
2025-02-02 16:58:12 +0100

    snippets: - code more robust

--- snippets/sitemap.php
+++ snippets/sitemap.php
@@ -3,7 +3,7 @@
 namespace PgFactory\PageFactory;
 
 $macroName = basename(__FILE__, '.php');
-$cachePrefix = $macroName . md5($args);
+$cachePrefix = $macroName . md5($args??'');
 
 if ($res = Cache::checkPageCache($cachePrefix)) {
     echo $res;
