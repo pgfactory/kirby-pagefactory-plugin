1af1e48f1a865abf3e0cc03b5e9f027711f95cb5
Dieter Stokar <zurisee@gmail.com>
2025-07-01 16:14:00 +0200

    Scss: - fix in resolvePaths() -> optional quotes in url(~/...) expressions

--- src/Scss.php
+++ src/Scss.php
@@ -189,9 +189,9 @@ class Scss
     private static function resolvePaths(string $html): string
     {
         // special case: url(~/ -> need to get url from pagefactory:
-        if (preg_match_all('|url\(\'?~/([^\s"\')]*)|', $html, $m)) {
-            foreach ($m[1] as $i => $item) {
-                $html = str_replace($m[0][$i], 'url('.PFY_APP_BASE_URL.$m[1][$i], $html);
+        if (preg_match_all('|url\((\'?)~/([^\s"\')]*)|', $html, $m)) {
+            foreach ($m[1] as $i => $q) {
+                $html = str_replace($m[0][$i], "url($q".PFY_APP_BASE_URL.$m[2][$i], $html);
             }
         }
 
