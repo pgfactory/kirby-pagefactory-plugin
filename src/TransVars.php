2db7644d1d101ba0cfd27e7b358a23f4f09ce570
Dieter Stokar <zurisee@gmail.com>
2025-08-13 15:54:53 +0200

    TransVar: - fix case missing var definition

--- src/TransVars.php
+++ src/TransVars.php
@@ -446,7 +446,7 @@ class TransVars
                     }
                 }
             }
-            if (str_contains($value, '\\ ')) {
+            if ($value !== null && str_contains($value, '\\ ')) {
                 $value = str_replace('\\ ', '&nbsp;', $value);
             }
             if ($doShield) {
