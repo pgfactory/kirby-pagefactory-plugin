1b002f65d11df0f0660defeff49d8c5789b20a63
Dieter Stokar <zurisee@gmail.com>
2025-11-03 08:38:03 +0100

    Img() macro added as snippet

---
+++ snippets/img.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PgFactory\PageFactory;
+
+$macroName = basename(__FILE__, '.php');
+$cachePrefix = $macroName . md5($args??'');
+
+$res = Cache::checkPageCache($cachePrefix);
+if ($res !== false) {
+    echo $res;
+    return;
+}
+Macros::instantiateMacroLoader($macroName, dirname(__DIR__) . "/macros/$macroName.php");
+
+if (!isset($args)) {
+    $args = '';
+} elseif (is_array($args)) {
+    $args = var_export($args, true);
+}
+$args .= ',lazyLoading: false, quickzoom: false';
+$res = Macros::execute($macroName, $args);
+
+Cache::updatePageCache($res, $cachePrefix);
+
+echo $res;
