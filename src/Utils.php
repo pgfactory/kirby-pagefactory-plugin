3ac835e4c4736e1f857d9c39843fbf03cfdb69e1
Dieter Stokar <inbox@usility.ch>
2026-03-17 16:58:31 +0100

    fix re PHP 8.5 compatibility

--- src/Utils.php
+++ src/Utils.php
@@ -1374,7 +1374,6 @@ EOT;
         curl_setopt($ch, CURLOPT_URL, "https://ipapi.co/timezone");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
-        curl_close($ch);
         return $output;
     } // getServerTimezone
 
--- src/helper.php
+++ src/helper.php
@@ -2851,7 +2851,7 @@ function iconExists(string $iconName): bool
     // check for digits-only, except if first char is '0':
     if (($value[0] !== '0') && !preg_match('/[^0-9.]+/', $value)) {
         if(preg_match('/[.]+/', $value)) {
-            return (double)$value;
+            return (float)$value;
         }else{
             return (int)$value;
         }
