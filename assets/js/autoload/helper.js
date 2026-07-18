36f451841de92f190706eb8ea690e59564449f7a
Dieter Stokar <inbox@usility.ch>
2026-07-18 21:24:10 +0200

    helper.js: - new functions str..capitalize() and stripHtml(str)

--- assets/js/autoload/helper.js
+++ assets/js/autoload/helper.js
@@ -377,3 +377,16 @@ function pfyHandleEvent(selector, func, trigger = 'click', containerEl = null) {
   });
 } // pfyHandleEvent
 
+
+String.prototype.capitalize = function() {
+    return this.replace(/(^|\s)([a-z])/g,
+      function(m, p1, p2) {
+          return p1 + p2.toUpperCase();
+      });
+}; // capitalize
+
+
+function stripHtml(html) {
+   const doc = new DOMParser().parseFromString(html, 'text/html');
+   return doc.body.textContent || "";
+} // stripHtml
